e5bc45c3db707ae2e2bc08f047215bf79a155339
ballountopp1 <ballountop@gmail.com>
2022-12-12 13:51:20 -0800

    5-printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,10 +4,9 @@
  * main -Entry
  * Return: Always 0
  */
-
 int main(void)
 {
-         printf("with proper grammar, but the outcome is a piece of art,\n");
-         return (s);
+	 printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (s);
 
-}	 
+}
