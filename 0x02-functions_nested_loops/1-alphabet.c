c4217aa8ded720b7232d3f4a38a9bf2c5579c812
ballountopp1 <ballountop@gmail.com>
2022-12-14 07:29:57 -0800

    1-alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,19 @@
 #include "main.h"
 
 /**
- * print_alphabet - Entry point
+ * print_alphabet -make the alphabet
  * Return:0
  */
 
 void print_alphabet(void)
+
 {
-	char c = 'a';
 
-	while (c <= 'z');
+	char c;
+
+	for  (c = 'a'; c <= 'z'; c++)
 	{
-		_putchar(c);
-
-		c++;
+	_putchar(c);
 	}
 	_putchar('\n');
 }
