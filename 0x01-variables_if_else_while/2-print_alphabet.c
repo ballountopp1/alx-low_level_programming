53c0a3029339beddf6433d8c1377914b361e52e7
ballountopp1 <ballountop@gmail.com>
2022-12-23 02:56:00 -0800

    task1

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * main - returs alpha
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+	int ch = 'a';
+	while (ch <= 'z');
+	{
+		putchar(ch);
+		ch++;
+	}
+		putchar('\n');
+	return (0);
+}
