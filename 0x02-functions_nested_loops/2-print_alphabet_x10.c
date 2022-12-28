ad6404ef0849898393909d67cb0af28a5bab79bf
ballountopp1 <ballountop@gmail.com>
2022-12-28 07:07:44 -0800

    task2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,17 +2,24 @@
 
 /**
  * print_alphabet_x10 -prints 10 times the alphabet, in lowercase
+ *
+ * Return: nothing on success.
  */
 
 void print_alphabet_x10(void)
 {
-	int count = 0;
-	char letter;
+	int start = 97, end = 122, count = 1;
 
-	while (count++ <= 9)
+	while (count <= 10)
 	{
-		for (letter = 'a'; letter <= 'z'; letter++)
-			_putchar = (letter);
-		_putchar('/n');
+		while (star <= end)
+		{
+			_putchar(start);
+			start++;
+		}
+		_putchar('\n');
+
+		start = 97;
+		count++;
 	}
 }
