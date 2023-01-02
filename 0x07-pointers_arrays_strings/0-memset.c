89dd0e8c4141d79a2cd585232115329e2988d6d6
ballountopp1 <ballountop@gmail.com>
2023-01-02 14:50:55 -0800

    task0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,16 +5,16 @@
  * @s: source string
  * @b: the constant byte for filling
  * @n: lenght of buffer
- * Return: new string.
+ * Return: changed array with new value for n bytes
  */
-char *_memset(char *s, char b, unsigned int n);
+char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int i = 0;
+	int i = 0;
 
-	while (i < n)
+	for (; n > 0; i++)
 	{
-		*(s + i) = b;
-		i++;
+		s[i] = b;
+		n--;
 	}
 	return (s);
 }
