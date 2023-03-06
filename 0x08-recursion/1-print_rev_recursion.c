6477f620956351c53d7ccba4c6dfcb9234830c90
ballountopp1 <ballountop@gmail.com>
2023-03-06 13:47:11 -0800

    updated

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,13 +1,11 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - Prints a string in reverse
- * @s: The string to be printed.
+ * _print_rev_recursion - prints a string in reverse
+ * @s: string to print
  */
 void _print_rev_recursion(char *s)
 {
-	int longit = 0;
-
 	if (*s)
 	{
 		_print_rev_recursion(s + 1);
